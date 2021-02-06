19d7f86084c3c4354bc0a028a1b51c38990bd2d4
eduardo reta rj <https://github.com/Eddie-Reta>
2021-02-05 23:40:08 -0500

    Created promises to catch data from the band api and spotify api. The data is now being passed to the user in the console.

--- js/liri.js
+++ js/liri.js
@@ -1,77 +1,91 @@
 require("dotenv").config();
-var inquirer = require("inquirer");
 
+var moment = require("moment");
+var axios = require("axios");
+//var inquirer = require("inquirer");
 var keys = require("./keys.js");
-//console.log(keys)
-var Spotify = require('node-spotify-api');
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
-
 var search = process.argv[2];
+var spotifyURI = `https://accounts.spotify.com/authorize?${keys.spotify.id}`;
+var command = process.argv[2];
+var input = process.argv[3];
+var bandApi =
+  "https://rest.bandsintown.com/artists/" +
+  input +
+  "/events?app_id=codingbootcamp";
 
-   spotify.search({type: 'track', query: search, limit: 1,}, function(err, response){
-     if (err) {
-       return console.log('Error occured: ' + err);
-     } 
-     
+switch (command) {
+  case "concert-this":
+    {
+      axios
+        .get(bandApi)
+        .then(function (response) {
+          var info = response.data;
 
-     console.log("Name: ");
-     console.log(response.tracks.items[0].artists[0].name);
+          console.log(
+            "\n Here are the upcoming concerts:" +
+              "\n" +
+              "-------------------------------------------------------" +
+              "\n" +
+              "                                                        "
+          );
 
-     console.log("Track: ");
-     console.log(response.tracks.items[0].name);
+          for (var i = 0; i < info.length; i++) {
+            var venue = info[i].venue;
+            var date = info[i].datetime;
 
-     console.log("Preview Link: ");
-     console.log(response.tracks.items[0].artists[0].uri);
+            console.log(
+              `\nPlace: ${venue.name} \n\nLocation: ${
+                venue.location
+              } \n\nDate: ${moment(date).format(
+                "MM-DD-YYYY"
+              )} \n\n---------------------------------------------------------------`
+            );
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+    break;
+  case "spotify-this-song":
+    {
+      spotify
+        .search({query: input , type: "track", limit: 1})
+        .then(function (response) {
+          var tracks = response.tracks.items;
 
-     console.log("Album: ");
-     console.log(response.tracks.items[0].album.name);
-     
-    // if (response){ console.log(JSON.stringify(response.items.name));
-   //}
-  });
-
-
-
-
-
-
-
-
-
-// inquirer.prompt([
-//   {
-//     type: "input",
-//     name: "name",
-//     message: "Type a song to get information?"
-//   }
-// ]).then(function(song){
-//   spotify.search({ type: 'track', query: 'dreams', limit: 2,}, function(err, response){
-//     if (err) {
-//       return console.log('Error occured: ' + err);
-//     } else if (response){ console.log(response.data) 
-
-//     }
-//   });
-// });
-
-
-
-
-
-
-
-
-
-
-
-// {
-// spotify.search(
-//   { type: 'track', query: 'All the Small Things', limit: 2}
-//   )
-// .then(function(response) {
-//   console.log(response.data);
-// })
-// .catch(function(err) {
-//   console.log(err);
-// });
-// });
+          for (var i = 0; i < tracks.length; i++) {
+            var artist = tracks[i].artists;
+            var songsName = tracks[i].name;
+            var previewLink = tracks[i].external_urls.spotify;
+            var albumName = tracks[i].album.name;
+            //console.log(artist)
+            //console.log(songsName.name)
+            for (var i = 0; i < artist.length; i++) {
+              console.log(artist[i].name);
+            };
+          };
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    }
+    break;
+  case "movie-this":
+    {
+      console.log("movie");
+    }
+    break;
+  case "do-what-it-says":
+    {
+      console.log("do it");
+    }
+    break;
+  default:
+    {
+      console.log("Sorry that command doesn't exist!");
+    }
+    break;
+}
