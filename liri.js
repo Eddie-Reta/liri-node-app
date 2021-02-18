500e382488267acccb5a5c1480106679c7a3e7fc
eduardo reta rj <https://github.com/Eddie-Reta>
2021-02-17 22:37:33 -0500

    On the start prompt you are now required to type in a keyword. Movie section now takes in data from the api. Still working on getting the rotten tomato rating.

--- liri.js
+++ liri.js
@@ -14,33 +14,44 @@ var Spotify = require("node-spotify-api");
 var command = process.argv[2];
 //var input = process.argv[3];
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      message: "Please choose what you would like to do?",
-      choices: ["concert-this", "spotify-this-song", "movie-this"],
-      name: "action",
-    },
-  ])
-  .then((answers) => {
-    var choice = answers.action;
+function start() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Please choose what you would like to do?",
+        choices: ["concert-this", "spotify-this-song", "movie-this"],
+        name: "action",
+      },
+    ])
+    .then((answers) => {
+      var choice = answers.action;
 
-    if (choice === "concert-this") {
-      main("Please type in the artist name to find their concerts?", choice);
-    } else if (choice === "spotify-this-song") {
-      main("Please type in the name of the song you would like to search?", choice);
-    } else if (choice === "movie-this") {
-      main("Please type in the name of the movie you would like to search?", choice);
-    }
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
-    }
-  });
+      if (choice === "concert-this") {
+        main(
+          "Please type in the artist's name to find their concerts?",
+          choice
+        );
+      } else if (choice === "spotify-this-song") {
+        main(
+          "Please type in the name of the song you would like to search?",
+          choice
+        );
+      } else if (choice === "movie-this") {
+        main(
+          "Please type in the name of the movie you would like to search for?",
+          choice
+        );
+      }
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else when wrong
+      }
+    });
+}
 
 //Need question , user input and, data
 
@@ -59,89 +70,128 @@ function main(question, choice) {
       switch (choice) {
         case "concert-this":
           {
-            var bandApi =
-              "https://rest.bandsintown.com/artists/" +
-              searchInput +
-              "/events?app_id=codingbootcamp";
-            axios
-              .get(bandApi)
-              .then(function (response) {
-                var info = response.data;
-         
-                if (info == [].length) {
-                console.log("\n\nSorry there is no upcoming concerts for this artist.")
-                } else {
-                console.log(
-                  "\n Here are the upcoming concerts:" +
-                    "\n" +
-                    "-------------------------------------------------------" +
-                    "\n" +
-                    "                                                        "
-                );
+            if (searchInput === "") {
+              console.log(`\n\nPlease type in the artist's name! \n\n`);
+              start();
+            } else {
+              var bandApi =
+                "https://rest.bandsintown.com/artists/" +
+                searchInput +
+                "/events?app_id=codingbootcamp";
+              axios
+                .get(bandApi)
+                .then(function (response) {
+                  var info = response.data;
 
-                for (var i = 0; i < info.length; i++) {
-                  var venue = info[i].venue;
-                  var date = info[i].datetime;
+                  if (info == [].length) {
+                    console.log(
+                      "\n\nSorry there is no upcoming concerts for this artist."
+                    );
+                  } else {
+                    console.log(
+                      "\n Here are the upcoming concerts:" +
+                        "\n" +
+                        "-------------------------------------------------------" +
+                        "\n" +
+                        "                                                        "
+                    );
 
-                  console.log(
-                    `\nPlace: ${venue.name} \n\nLocation: ${
-                      venue.location
-                    } \n\nDate: ${moment(date).format(
-                      "MM-DD-YYYY"
-                    )} \n\n---------------------------------------------------------------`
-                  );
-                };
-                };
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
+                    for (var i = 0; i < info.length; i++) {
+                      var venue = info[i].venue;
+                      var date = info[i].datetime;
+
+                      console.log(
+                        `\nPlace: ${venue.name} \n\nLocation: ${
+                          venue.location
+                        } \n\nDate: ${moment(date).format(
+                          "MM-DD-YYYY"
+                        )} \n\n---------------------------------------------------------------`
+                      );
+                    }
+                  }
+                })
+                .catch(function (error) {
+                  console.log(error);
+                });
+            }
           }
           break;
         case "spotify-this-song":
           {
             var spotify = new Spotify(keys.spotify);
-4
-            spotify
-              .search({ query: searchInput, type: "track", limit: 1 })
-              .then(function (response) {
 
-                var tracks = response.tracks.items;
+            if (searchInput === "") {
+              console.log(`\n\nPlease type in a track name!\n\n`);
+              start();
+            } else {
+              spotify
+                .search({ query: searchInput, type: "track", limit: 1 })
+                .then(function (response) {
+                  var tracks = response.tracks.items;
 
-                if (tracks == [].length) {
-                  console.log("\nSorry we couldn't find song. \nHere you should listen to The Sign by Ace of Base.");
-                } else {
-              
-                var art = [];
+                  if (tracks == [].length || tracks == "") {
+                    console.log(
+                      "\nSorry we couldn't find song. \nHere you should listen to The Sign by Ace of Base."
+                    );
+                  } else {
+                    var art = [];
 
-                for (var i = 0; i < tracks.length; i++) {
-                  var artist = tracks[i].artists;
-                  var songsName = tracks[i].name;
-                  var previewLink = tracks[i].external_urls.spotify;
-                  var albumName = tracks[i].album.name;
+                    for (var i = 0; i < tracks.length; i++) {
+                      var artist = tracks[i].artists;
+                      var songsName = tracks[i].name;
+                      var previewLink = tracks[i].external_urls.spotify;
+                      var albumName = tracks[i].album.name;
 
-                  for (var i = 0; i < artist.length; i++) {
-                  art.push(" " + artist[i].name)
+                      for (var i = 0; i < artist.length; i++) {
+                        art.push(" " + artist[i].name);
+                      }
+
+                      console.log(
+                        `\nHere is the track ${songsName}. \n\nThese are the artist on the track ${art}. \n\nThis track belongs to this album ${albumName}. \n\nHere is a preview link from Spotify ${previewLink}.`
+                      );
+                    }
                   }
-                 
-                  console.log(`\nHere is the track ${songsName}. \n\nThese are the artist on the track ${art}. \n\nThis track belongs to this album ${albumName}. \n\nHere is a preview link from Spotify ${previewLink}.`);
-               
-                }
-              };
-              })
-              .catch(function (err) {
-                console.log(err);
-              });
+                })
+                .catch(function (err) {
+                  console.error("there is error");
+                });
+            }
           }
           break;
         case "movie-this":
           {
-              var pass = keys.movie
-            if (res == "" || res == [].length) {
-              console.log("nOPE")
+            //take in password from env file
+            
+            var pass = keys.movie;
+
+            // Take in promp answer for movie
+            if (searchInput === "") {
+              console.log(
+                `\n\nPlease type in the name of movie you would like to know about? \n\n`
+              );
+              start();
             } else {
-              
-            };
+              var omdb = `http://www.omdbapi.com/?apikey=${pass.secret}&t=${searchInput}&tomatoes=true`;
+
+              axios
+                .get(omdb)
+                .then(function (response) {
+                  var data = response.data;
+                  var newReleasedDate = moment(data.Released, "MM/DD/YYYY");
+                  function tomatoRating(){
+                    var tomatoR = data.Ratings;
+
+                    for (var i = 0; i < tomatoR.length; i++) {
+                      console.log(tomatoR[i]);
+                    }
+                  }
+                  tomatoRating();
+                  console.log(`\nMovie Title: ${data.Title} \n\nReleased Date: ${moment(newReleasedDate).format("MM-DD-YYYY")} \n\nIMDB Rating: ${data.imdbRating} \n\nRotten Tomatoes Rating: \n\nCountry: ${data.Country} \n\nLanguage of the Movie: ${data.Language} \n\nPlot: ${data.Plot} \n\nActors: ${data.Actors}`);
+                })
+                .catch(function (err) {
+                  console.log(err);
+                });
+            }
           }
           break;
         default:
@@ -155,3 +205,5 @@ function main(question, choice) {
       console.log(error);
     });
 }
+
+start();
